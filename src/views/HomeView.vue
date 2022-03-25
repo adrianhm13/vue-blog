7f08413d3799fd6823e444639c99be3539aa71c9
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-25 09:13:20 +0100

    watch, watchEffect use case

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,44 @@
 <template>
-  <div class="home"></div>
-  <h2>Refs</h2>
-  <p>My name is {{ userOne.name }}, my age is {{ userOne.age }}</p>
-  <button @click="updateUserOne">Click me</button>
-  <h2>Reactive</h2>
-  <p>{{ userTwo.name }} - {{ userTwo.age }}</p>
-  <button @click="updateUserTwo">Update User 2</button>
+  <div class="home">
+    <h1>Home</h1>
+    <input type="text" v-model="search" />
+    <p>Search Term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
+    <hr />
+    <div v-for="name in names" :key="name">
+      {{ name }}
+    </div>
+    <button @click="handleClick">stop watching</button>
+  </div>
 </template>
 
 <script>
-import { ref, reactive } from "@vue/reactivity";
+import { ref, computed, watch, watchEffect } from "vue";
 
 export default {
   name: "HomeView",
   setup() {
-    const userOne = ref({ name: "Adrian", age: 25 });
-    const userTwo = reactive({ name: "Joseee", age: 55 });
+    const names = ref(["adrian", "ana", "mario", "jose", "fabio", "tristan"]);
+    const search = ref("");
 
-    // Ref
-    const updateUserOne = () => {
-      userOne.value.age = 30;
+    const stopWatch = watch(search, () => {
+      console.log("Search has changed", search.value);
+    });
+
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect fired", search.value);
+    });
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    const handleClick = () => {
+      stopWatch();
+      stopEffect();
     };
-
-    //Reactive
-    const updateUserTwo = () => {
-      userTwo.name = 'Adrian'
-      userTwo.age = 30;
-    };
-
-    return { userOne, updateUserOne, updateUserTwo, userTwo };
+    return { names, search, matchingNames, handleClick };
   },
 };
 </script>
