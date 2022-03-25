63aada744eeba0c5b75aede87098e6c659baeba0
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-25 10:23:15 +0100

    Added post components

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from "vue";
+import SinglePost from "./SinglePost";
+
+export default {
+  props: ["posts"],
+  components: { SinglePost },
+  setup(props) {
+    console.log("Props", props);
+    onMounted(() => console.log("Component mounted"));
+    onUnmounted(() => console.log("Component unmounted"));
+    onUpdated(() => console.log("Component updated"));
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search" />
-    <p>Search Term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      {{ name }}
-    </div>
-    <hr />
-    <div v-for="name in names" :key="name">
-      {{ name }}
-    </div>
-    <button @click="handleClick">stop watching</button>
+    <PostList :posts="posts" v-if="showPosts" />
+    <button @click="showPosts = !showPosts">Toggle Posts</button>
+    <button @click="posts.pop()">Delete post</button>
   </div>
 </template>
 
 <script>
-import { ref, computed, watch, watchEffect } from "vue";
+import { ref } from "vue";
+import PostList from "../components/PostList.vue";
 
 export default {
   name: "HomeView",
+  components: { PostList },
   setup() {
-    const names = ref(["adrian", "ana", "mario", "jose", "fabio", "tristan"]);
-    const search = ref("");
-
-    const stopWatch = watch(search, () => {
-      console.log("Search has changed", search.value);
-    });
-
-    const stopEffect = watchEffect(() => {
-      console.log("watchEffect fired", search.value);
-    });
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value));
-    });
-
-    const handleClick = () => {
-      stopWatch();
-      stopEffect();
-    };
-    return { names, search, matchingNames, handleClick };
+    const showPosts = ref(true);
+    const posts = ref([
+      { title: "welcome to the blog", body: "Lorem Ipsum", id: 1 },
+      {
+        title: "A new post in this wonderful blog",
+        body: "Lorem Ipsdum",
+        id: 2,
+      },
+    ]);
+    return { posts, showPosts };
   },
 };
 </script>
