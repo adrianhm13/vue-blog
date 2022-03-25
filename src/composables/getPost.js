4e785d5be988a0c57728264bba78b3358b30d9f4
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-25 13:02:17 +0100

    Fetch single post

---
+++ src/composables/getPost.js
@@ -0,0 +1,29 @@
+import { ref } from "vue";
+
+const getPost = () => {
+  const post = ref(null);
+  const error = ref(null);
+
+  const load = async (id) => {
+    try {
+      // simulate delay
+      await new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+      
+      let data = await fetch(`http://localhost:3000/posts/${id}`);
+
+      if (!data.ok) {
+        throw Error("No post have been found");
+      }
+      post.value = await data.json();
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
+    }
+  };
+
+  return { load, error, post };
+};
+
+export default getPost;
